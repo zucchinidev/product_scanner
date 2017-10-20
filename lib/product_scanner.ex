ac3e6e4fe63b1add1f5acfcff0660e67dc830e24
Andrea Zucchini <azga84@gmail.com>
2017-10-20 21:42:55 +0200

    Initial commit of the project

---
+++ lib/product_scanner.ex
@@ -0,0 +1,18 @@
+defmodule ProductScanner do
+  @moduledoc """
+  Documentation for ProductScanner.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProductScanner.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/product_scanner_test.exs
@@ -0,0 +1,8 @@
+defmodule ProductScannerTest do
+  use ExUnit.Case
+  doctest ProductScanner
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
