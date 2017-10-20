04298507cd05e350946e2e77637327cf166e8478
Andrea Zucchini <azga84@gmail.com>
2017-10-21 00:06:01 +0200

    verify available discounters

--- test/discounters/register_discounters_test.exs
+++ test/discounters/register_discounters_test.exs
@@ -1,13 +1,13 @@
-defmodule ProductScanner.Discounters.DefaultDiscounterTest do
+defmodule ProductScanner.Discounters.RegisterDiscountersTest do
   use ExUnit.Case
-  doctest ProductScanner.Discounters.DefaultDiscounter
+  doctest ProductScanner.Discounters.RegisterDiscounters
+  alias ProductScanner.Discounters.{DefaultDiscounter,TwoForOneDiscounter}
 
-  import ProductScanner.Discounters.DefaultDiscounter, only: [
-    perform_discount: 1
+  import ProductScanner.Discounters.RegisterDiscounters, only: [
+    available_discounters: 0
   ]
 
-  test "should allow return a list of products witout modify" do
-    product = "fake_product"
-    assert perform_discount([product]) == [product]
+  test "should allow return a struct with one list of discounters modules" do
+    assert available_discounters() == [DefaultDiscounter, TwoForOneDiscounter]
   end
 end
