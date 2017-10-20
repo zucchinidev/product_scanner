b8760e4790dad9ec573964aa0a712f59d5e07c9e
Andrea Zucchini <azga84@gmail.com>
2017-10-21 00:07:24 +0200

    verify the data required for the operation of the module

--- test/product_scanner_test.exs
+++ test/product_scanner_test.exs
@@ -3,8 +3,17 @@ defmodule ProductScannerTest do
   doctest ProductScanner
 
   import ProductScanner, only: [
-    scan: 1
+    scan: 1,
+    init: 0
   ]
+
+  setup do
+    {:ok, Map.from_struct(init())}
+  end
+
+  test "should have two discount modules", state do
+    assert length(state.discounters) == 2
+  end
 
   test "should allow a scan to valid product" do
     product = "fake_product"
