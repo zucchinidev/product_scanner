81ff1ee90c8c13202cca1758be70d6393e0995d5
Andrea Zucchini <azga84@gmail.com>
2017-10-20 23:04:49 +0200

    create the skeleton of the module of the discount two for one

---
+++ lib/product_scanner/discounters/two_for_one_discounter.ex
@@ -0,0 +1,16 @@
+defmodule ProductScanner.Discounters.TwoForOneDiscounter do
+  @moduledoc """
+  This module performs operations to realize the discount two by one.
+  """
+
+  @doc """
+  Applies the discount two for one to the list of products
+    ## Examples
+
+    iex> ProductScanner.Discounters.DefaultDiscounter.perform_discount(["VOUCHER"])
+    ["VOUCHER"]
+  """
+  def perform_discount(products) do
+    products
+  end
+end
