2abf7607b86d56007dbe9151635dd40605623171
Andrea Zucchini <azga84@gmail.com>
2017-10-20 22:33:18 +0200

    verify that a valid product is inserted

--- test/product_scanner_test.exs
+++ test/product_scanner_test.exs
@@ -2,7 +2,21 @@ defmodule ProductScannerTest do
   use ExUnit.Case
   doctest ProductScanner
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import ProductScanner, only: [
+    scan: 1
+  ]
+
+  test "should allow a scan to valid product" do
+    product = "fake_product"
+    assert scan(product) == product
+  end
+
+  test "should throw an ArgumentError when the product is nil or empty" do
+    invalid_products = ["", nil]
+    invalid_products |> Enum.each(fn product ->
+      assert_raise ArgumentError, fn ->
+        scan(product)
+      end
+    end)
   end
 end
