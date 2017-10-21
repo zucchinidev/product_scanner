2715cd662cc5426057b480fb2a6a4971100d546d
Andrea Zucchini <azga84@gmail.com>
2017-10-22 00:04:14 +0200

    create product repository

--- lib/product/infrastructure/product_repository.ex
+++ lib/product/infrastructure/product_repository.ex
@@ -1,34 +1,24 @@
 defmodule Product.Infrastructure.ProductRepository do
   @moduledoc """
-  Logic that handle the products list
+  Module that handle the available products list
   """
-
   use GenServer
 
+  import Product
+
   def start_link do
-    GenServer.start_link(__MODULE__, [])
-  end
-
-  def count(pid) do
-    GenServer.call(pid, :count)
-  end
-
-  def add(pid, product) do
-    GenServer.cast(pid, {:add, product})
+    {:ok, products_data} = "./products.json"
+      |> Path.expand(__DIR__)
+      |> File.read()
+    products = Poison.decode!(products_data, as: [%Product{}])
+    GenServer.start_link(__MODULE__, products)
   end
 
   def get_all(pid) do
     GenServer.call(pid, :get_all)
   end
 
-  def handle_call(:count, _from, products) do
-    {:reply, Enum.count(products), products}
-  end
   def handle_call(:get_all, _from, products) do
     {:reply, products, products}
-  end
-
-  def handle_cast({:add, product}, products) do
-    {:noreply, [product | products]}
   end
 end
