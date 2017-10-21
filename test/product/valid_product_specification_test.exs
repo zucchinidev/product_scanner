10b1dab06ddd59febe61988f051bdfe9608c4b4f
Andrea Zucchini <azga84@gmail.com>
2017-10-21 13:21:46 +0200

    verification of domain rules applied to products

---
+++ test/product/valid_product_specification_test.exs
@@ -0,0 +1,20 @@
+defmodule ProductScanner.Product.ValidProductSpecificationTest do
+  use ExUnit.Case
+  doctest ProductScanner.Product.ValidProductSpecification
+
+  import ProductScanner.Product.ValidProductSpecification, only: [
+    is_satisfied_by: 1
+  ]
+
+  test "should check if a product is valid" do
+    product = "fake_product"
+    assert is_satisfied_by(product) == true
+  end
+
+  test "should should check if a product is not valid" do
+    invalid_products = ["", nil]
+    invalid_products |> Enum.each(fn product ->
+      assert is_satisfied_by(product) == false
+    end)
+  end
+end
