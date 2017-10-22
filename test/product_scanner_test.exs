241c35cd82787563b05b2a19a3f5b006bba9615c
Andrea Zucchini <azga84@gmail.com>
2017-10-22 16:15:24 +0200

    verify method to recover all scanned products

--- test/product_scanner_test.exs
+++ test/product_scanner_test.exs
@@ -1,31 +1,33 @@
 defmodule ProductScannerTest do
   use ExUnit.Case
-
   import ProductScanner, only: [
-    start_link: 0,
-    scan: 2
+    start_link: 1,
+    scan: 2,
+    get_scanned_products: 1
   ]
 
-  test "should have modules to apply discounts" do
-    {:ok, pid} = start_link()
-    %ProductScanner{discounters: discounters} = :sys.get_state(pid)
-
-    assert is_map(discounters)
-    assert is_atom(discounters.default)
-    assert function_exported?(discounters.default, :perform_discount, 1)
+  setup do
+    {:ok, pid} = ProductScanner.StateCreator.create() |> start_link()
+    {:ok, %{pid: pid}}
   end
 
-  test "should allow a scan a product" do
-    {:ok, pid} = start_link()
+  test "should allow a scan a product", %{pid: pid} do
     product = "fake_product"
     {:ok, _state} = scan(pid, product)
   end
 
-  test "should verify if any product inserted is not valid" do
-    {:ok, pid} = start_link()
+  test "should verify if any product inserted is not valid", %{pid: pid} do
     invalid_products = ["", nil]
     invalid_products |> Enum.each(fn product ->
       {:error, _msg} = scan(pid, product)
     end)
+  end
+
+  test "should return all scanned products", %{pid: pid} do
+    products = ["fake_product_one", "fake_product_two"]
+    products |> Enum.each(fn product ->
+      {:ok, _state} = scan(pid, product)
+    end)
+    assert get_scanned_products(pid) |> length() == 2
   end
 end
