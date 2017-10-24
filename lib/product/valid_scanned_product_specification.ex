37cf72866cee7b197ffeda27fbde1b911bb77dc4
Andrea Zucchini <azga84@gmail.com>
2017-10-24 22:00:51 +0200

    improve valid scanned product specification

--- lib/product/valid_scanned_product_specification.ex
+++ lib/product/valid_scanned_product_specification.ex
@@ -1,4 +1,4 @@
-defmodule Product.ValidProductSpecification do
+defmodule Product.ValidScannedProductSpecification do
   @moduledoc """
   This module validate a product to see if it fulfils the set of rules of domain
   """
@@ -7,21 +7,26 @@ defmodule Product.ValidProductSpecification do
   Allows verification of the domain rules applied to the products
     ## Examples
 
-    iex> Product.ValidProductSpecification.is_satisfied_by("VOUCHER")
+    iex> Product.ValidScannedProductSpecification.is_satisfied_by("VOUCHER", [%Product{code: "VOUCHER"}])
     true
   """
-  def is_satisfied_by(product) do
-    Enum.all?(valid_product_rules(product))
+  def is_satisfied_by(product_code, products_allowed) do
+    Enum.all?(valid_product_rules(product_code, products_allowed))
   end
 
-  defp valid_product_rules(product_name) do
+  defp valid_product_rules(product_code, products_allowed) do
     [
-      product_not_nil(product_name),
-      product_not_empty(product_name)
+      product_not_nil(product_code),
+      product_not_empty(product_code),
+      is_allowed(product_code, products_allowed)
     ]
   end
 
   defp product_not_nil(product), do: product != nil
 
   defp product_not_empty(product), do: product != ""
+
+  defp is_allowed(product_code, products_allowed) do
+    Enum.find(products_allowed, fn p -> p.code == product_code end)
+  end
 end
--- test/product/valid_scanned_product_specification_test.exs
+++ test/product/valid_scanned_product_specification_test.exs
@@ -1,20 +1,26 @@
-defmodule Product.ValidProductSpecificationTest do
+defmodule Product.ValidScannedProductSpecificationTest do
   use ExUnit.Case
-  doctest Product.ValidProductSpecification
+  doctest Product.ValidScannedProductSpecification
 
-  import Product.ValidProductSpecification, only: [
-    is_satisfied_by: 1
+  import Product.ValidScannedProductSpecification, only: [
+    is_satisfied_by: 2
   ]
 
   test "should check if a product is valid" do
-    product = "fake_product"
-    assert is_satisfied_by(product) == true
+    fake_product = "fake_product"
+    product = %Product{
+      code: "fake_product"
+    }
+    assert is_satisfied_by(fake_product, [product]) == true
   end
 
   test "should check if a product is not valid" do
     invalid_products = ["", nil]
+    valid_product = %Product{
+      code: "fake_product"
+    }
     invalid_products |> Enum.each(fn product ->
-      assert is_satisfied_by(product) == false
+      assert is_satisfied_by(product, [valid_product]) == false
     end)
   end
 end
