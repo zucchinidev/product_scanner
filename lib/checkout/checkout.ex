7c12de355a3898e09da0be8d4722c03d731f0bb4
Andrea Zucchini <azga84@gmail.com>
2017-10-24 22:10:19 +0200

    change variable name

--- lib/checkout/checkout.ex
+++ lib/checkout/checkout.ex
@@ -3,7 +3,7 @@ defmodule Checkout do
 
   alias __MODULE__, as: Checkout
 
-  defstruct ~w(scanned_products product_respository available_discounters filtered_products)a
+  defstruct ~w(scanned_products product_respository available_discounters segmented_products)a
 
   def calculate_total_amount(%Checkout{} = state) do
     state
@@ -18,31 +18,31 @@ defmodule Checkout do
   end
 
   defp get_products_with_discounted(%Checkout{scanned_products: scanned_products} = state) do
-    discounted_products = Enum.reduce(get_rules(), %{}, fn ({key, rule}, acc) ->
+    segmented_products = Enum.reduce(get_rules(), %{}, fn ({key, rule}, acc) ->
       related_products = rule[:related_products]
       filter_products = Enum.filter(scanned_products, fn product ->
         Enum.member?(related_products, product.code)
       end)
       Map.put(acc, key, filter_products)
     end)
-    %Checkout{state | filtered_products: discounted_products}
+    %Checkout{state | segmented_products: segmented_products}
   end
 
   defp get_products_with_default_discounted(%Checkout{
-    scanned_products: scanned_products, filtered_products: filtered_products} = state) do
+    scanned_products: scanned_products, segmented_products: segmented_products} = state) do
     default_products = Enum.filter(scanned_products, fn product ->
-      condition = Enum.reduce(filtered_products, true, fn ({_key, related_products}, acc) ->
+      condition = Enum.reduce(segmented_products, true, fn ({_key, related_products}, acc) ->
         acc && !Enum.member?(related_products, product)
       end)
       condition
     end)
-    discounted_products = Map.put(filtered_products, :default, default_products)
-    %Checkout{state | filtered_products: discounted_products}
+    segmented_products = Map.put(segmented_products, :default, default_products)
+    %Checkout{state | segmented_products: segmented_products}
   end
 
   defp sum_amount(%Checkout{
-    available_discounters: discounters, filtered_products: filtered_products}) do
-    Enum.reduce(filtered_products, 0, fn ({discounter, products}, acc) ->
+    available_discounters: discounters, segmented_products: segmented_products}) do
+    Enum.reduce(segmented_products, 0, fn ({discounter, products}, acc) ->
       acc + apply(discounters[discounter], :perform_discount, [products])
     end)
   end
