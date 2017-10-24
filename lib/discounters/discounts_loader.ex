2a70aa300b9b33d88720b34d06bb8f07eb3dd1b4
Andrea Zucchini <azga84@gmail.com>
2017-10-24 21:52:35 +0200

    add three or more discounter

--- lib/discounters/discounts_loader.ex
+++ lib/discounters/discounts_loader.ex
@@ -2,12 +2,17 @@ defmodule Discounters.DiscountsLoader do
   @moduledoc """
   This module register all discounters available.
   """
-  alias Discounters.{DefaultDiscounter,TwoForOneDiscounter}
+  alias Discounters.{
+    DefaultDiscounter,
+    TwoForOneDiscounter,
+    ThreeOrMoreDiscounter
+  }
 
   def available_discounters do
     %{
       default: DefaultDiscounter,
-      two_for_one: TwoForOneDiscounter
+      two_for_one: TwoForOneDiscounter,
+      three_or_more: ThreeOrMoreDiscounter
     }
   end
 
--- test/discounters/discounts_loader_test.exs
+++ test/discounters/discounts_loader_test.exs
@@ -1,7 +1,11 @@
 defmodule Discounters.LoaderTest do
   use ExUnit.Case
   doctest Discounters.DiscountsLoader
-  alias Discounters.{DefaultDiscounter,TwoForOneDiscounter}
+  alias Discounters.{
+    DefaultDiscounter,
+    TwoForOneDiscounter,
+    ThreeOrMoreDiscounter
+  }
 
   import Discounters.DiscountsLoader, only: [
     available_discounters: 0
@@ -10,7 +14,8 @@ defmodule Discounters.LoaderTest do
   test "should allow return a struct with one list of discounters modules" do
     assert available_discounters() == %{
               default: DefaultDiscounter,
-              two_for_one: TwoForOneDiscounter
+              two_for_one: TwoForOneDiscounter,
+              three_or_more: ThreeOrMoreDiscounter
            }
   end
 end
